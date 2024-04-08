8f4033675f321432c70717f97707bcf658b0f7db
yinchaogao <yinchaogao@deeproute.ai>
2024-04-08 13:40:17 +0800

    update

--- examples/print_hook.cc
+++ examples/print_hook.cc
@@ -1,12 +1,37 @@
+#include <cstdint>
 #include <iostream>
 
+#include "PerfMonitor.h"
 #include "uinspect.h"
+
+std::uint64_t page_fault = 0;
+std::uint64_t inst = 0;
+std::uint64_t cycle = 0;
+thread_local uinspect::PerfMonitor monitor = []() {
+  uinspect::PerfMonitor monitor;
+  monitor.Monitor(PERF_COUNT_SW_PAGE_FAULTS, &page_fault);
+  monitor.Monitor(PERF_COUNT_HW_CPU_CYCLES, &cycle);
+  monitor.Monitor(PERF_COUNT_HW_INSTRUCTIONS, &inst);
+  monitor.Reset();
+  return monitor;
+}();
 
 static void print_enter(uinspect::HookEntry* hook) {
   std::cout << "[uinspect] print enter" << std::endl;
+  monitor.Update();
+  monitor.Enable();
 }
 static void print_exit(uinspect::HookEntry* hook) {
+  monitor.Disable();
+  auto prev_fault = page_fault;
+  auto prev_cycle = cycle;
+  auto prev_inst = inst;
+  monitor.Update();
   std::cout << "[uinspect] print leave" << std::endl;
+  std::cout << "[uinspect] page fault: " << page_fault - prev_fault
+            << std::endl;
+  std::cout << "[uinspect] inst: " << inst - prev_inst << std::endl;
+  std::cout << "[uinspect] cycle: " << cycle - prev_cycle << std::endl;
 }
 
 UINSPECT_HOOK(":_Z5printv", print_enter, print_exit)
---
+++ src/PerfMonitor.h
@@ -0,0 +1,99 @@
+#pragma once
+#include <linux/perf_event.h>
+#include <sched.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include <cstdint>
+#include <cstring>
+#include <unordered_map>
+#include <vector>
+
+namespace uinspect {
+
+class PerfMonitor {
+  int fd_{-1};
+
+  std::unordered_map<std::uint64_t, std::uint64_t *> id2addr_;
+  std::vector<char> buf_;
+
+  int Monitor(struct perf_event_attr *hw_event, pid_t pid, int cpu,
+              unsigned long flags) {
+    int ret;
+    ret = syscall(SYS_perf_event_open, hw_event, pid, cpu, fd_,
+                  flags | PERF_FLAG_FD_CLOEXEC);
+    if (fd_ == -1 && ret != -1) {
+      fd_ = ret;
+    }
+    return ret;
+  }
+
+ public:
+  int Monitor(struct perf_event_attr *hw_event, std::uint64_t *val) {
+    hw_event->disabled = 1;
+    hw_event->inherit = 0;
+    hw_event->exclude_kernel = 1;
+    hw_event->exclude_hv = 1;
+    hw_event->exclude_guest = 1;
+    hw_event->exclude_idle = 1;
+
+    hw_event->read_format = PERF_FORMAT_GROUP | PERF_FORMAT_ID;
+
+    int ret = Monitor(hw_event, 0, -1, 0);
+    if (ret != -1) {
+      std::uint64_t id;
+      ioctl(ret, PERF_EVENT_IOC_ID, &id);
+      id2addr_.emplace(id, val);
+    }
+    return ret;
+  }
+  int Monitor(perf_sw_ids e, std::uint64_t *val) {
+    struct perf_event_attr pe;
+    memset(&pe, 0, sizeof(pe));
+
+    pe.size = sizeof(pe);
+    pe.type = PERF_TYPE_SOFTWARE;
+    pe.config = e;
+
+    return Monitor(&pe, val);
+  }
+  int Monitor(perf_hw_id e, std::uint64_t *val) {
+    struct perf_event_attr pe;
+    memset(&pe, 0, sizeof(pe));
+
+    pe.size = sizeof(pe);
+    pe.type = PERF_TYPE_HARDWARE;
+    pe.config = e;
+
+    return Monitor(&pe, val);
+  }
+
+  int Enable() {
+    return ioctl(fd_, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
+  }
+  int Disable() {
+    return ioctl(fd_, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
+  }
+  int Reset() { return ioctl(fd_, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP); }
+  ssize_t Update() {
+    struct read_format {
+      std::uint64_t val;
+      std::uint64_t id;
+    };
+    std::size_t size =
+        sizeof(std::uint64_t) + id2addr_.size() * sizeof(read_format);
+    buf_.resize(size);
+    ssize_t len = read(fd_, buf_.data(), buf_.size());
+    read_format *items =
+        reinterpret_cast<read_format *>(buf_.data() + sizeof(std::uint64_t));
+    for (std::uint64_t i = 0;
+         i < *reinterpret_cast<std::uint64_t *>(buf_.data()); i++) {
+      *id2addr_.at(items[i].id) = items[i].val;
+    }
+    return len;
+  }
+};
+
+}  // namespace uinspect
+
