4644de964d73d336fad27de0a46e292d27bcd40e
yinchaogao <yinchaogao@deeproute.ai>
2024-04-19 15:16:48 +0800

    use realpath

--- uinspect/SegmentManager.cc
+++ uinspect/SegmentManager.cc
@@ -1,5 +1,8 @@
 #include "SegmentManager.h"
 
+#include <cstdlib>
+#include <cstring>
+
 namespace uinspect {
 
 SegmentManager::SegmentManager() { Refresh(); }
@@ -15,12 +18,21 @@ void SegmentManager::Refresh() {
         SegmentManager* manager = (SegmentManager*)(self);
         for (ElfW(Half) i = 0; i < info->dlpi_phnum; i++) {
           if (info->dlpi_phdr[i].p_type == PT_LOAD) {
-            std::string name = info->dlpi_name;
             auto file_addr = info->dlpi_phdr[i].p_offset;
             auto file_sz = info->dlpi_phdr[i].p_filesz;
 
             auto mem_addr = info->dlpi_addr + info->dlpi_phdr[i].p_vaddr;
             auto mem_sz = info->dlpi_phdr[i].p_memsz;
+
+            std::string name;
+            if (strlen(info->dlpi_name)) {
+              char real_path[1024];
+              if (realpath(info->dlpi_name, real_path) == NULL) {
+                name = info->dlpi_name;
+              } else {
+                name = real_path;
+              }
+            }
 
             manager->name2segment[name].emplace(
                 file_addr, SegmentInfo{info->dlpi_name, file_addr, file_sz,
@@ -36,7 +48,14 @@ const SegmentInfo* SegmentManager::FindSegmentByFile(const char* name,
                                                      ElfW(Off) addr) {
   auto iter = name2segment.find(name);
   if (iter == name2segment.end()) {
-    return nullptr;
+    char real_path[1024];
+    if (realpath(name, real_path) == NULL) {
+      return nullptr;
+    }
+    iter = name2segment.find(real_path);
+    if (iter == name2segment.end()) {
+      return nullptr;
+    }
   }
   auto seg_iter = iter->second.lower_bound(addr);
   if (seg_iter == iter->second.end()) {
