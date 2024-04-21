f38b1029103e447b249944d0aea0eae53bb5459c
yinchaogao <yinchaogao@deeproute.ai>
2024-04-21 12:28:46 +0800

    add map_engine

---
+++ example/map_engine_hook.cc
@@ -0,0 +1,9 @@
+#include "common.h"
+
+UINSPECT_HOOK(
+    ":_ZN9deeproute10map_engine18MapEngineComponent4ProcERKSt13unordered_"
+    "mapINSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEESt10shared_"
+    "ptrIKN6google8protobuf7MessageEESt4hashIS8_ESt8equal_toIS8_"
+    "ESaISt4pairIKS8_SE_EEEPS2_IS8_St6vectorIS9_ISC_ESaISR_EESG_SI_SaISJ_ISK_"
+    "ST_EEE",
+    func_enter, func_exit)
